ffee3b43d8d5af9ed09841a7750bb1d42af059fa
alondra zurita <alondrazurita2@gmail.com>
2023-09-20 01:23:38 -0500

    added inquirer

--- server.js
+++ server.js
@@ -1,4 +1,75 @@
-const express = require('express');
-const mysql = require('mysql2');
-const PORT = process.env.PORT || 3001;
-const app = express();
+const fs = require('fs');
+const inquirer = require('inquirer');
+console.log("it work!");
+
+const mainMenuQuestion = [
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'menu',
+        choices: [
+            'View All Employees',
+            'Add Employee',
+            'Update Employee Role',
+            'View All Roles',
+            'Add Role',
+            'View All Departments',
+            'Add Department'
+        ]
+    },
+    {
+        type: 'input',
+        message: 'What is the name of the department',
+        name: 'Add department'
+    },
+    {
+        type: 'input',
+        message:'What is the name of the role?',
+        name: 'Add role'
+    },
+    {
+        type: 'input',
+        message: 'What is the salary of the role?',
+        name: 'add salary'
+    },
+    {
+        type: 'checkbox',
+        message: 'Which department does the role belong to?',
+        name: 'Choose department',
+        choices: [
+            'Engineering',
+            'Finance',
+            'Legal',
+            'Sales',
+            'Service'
+        ]
+    }
+
+];
+
+function init() {
+    inquirer.prompt(mainMenuQuestion)
+    .then((answer)=>{
+        switch (answer.menu) {
+            case 'View All Employees':
+                break;
+            case 'Add Employee':
+                break;
+            case 'Update Employee Role':
+                break;
+            case 'View All Roles':
+                break;
+            case 'Add Role':
+                break;
+            case 'View All Departments':
+                break;
+            case 'Add Department':
+                break;
+            default:
+                console.log('Invalid choice');
+                break;
+        }
+    })
+}
+
+init();
